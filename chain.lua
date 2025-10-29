7e34eb3cfa14bff8eb6c59469a6b3efb05b73533
Ilya Muravyev <ilya.yarn@gmail.com>
2025-10-29 18:21:02 -0400

    Update aliases

--- chain.lua
+++ chain.lua
@@ -2278,22 +2278,38 @@ function chain.range(startNum, finishNum, stepNum)
     return Chain.new(newXt)
 end
 
---- Aliases
+---- Aliases
+----
+--- Alias for `:getAtPositionOrNil`.
 Chain.at       = Chain.getAtPositionOrNil
+--- Alias for `:forEach`.
 Chain.each     = Chain.forEach
+--- Alias for `:forEach`.
 Chain.foreach  = Chain.forEach
+--- Alias for `:findFirstOrNil`.
 Chain.find     = Chain.findFirstOrNil
+--- Alias for `:findLastOrNil`.
 Chain.findLast = Chain.findLastOrNil
+--- Alias for `:firstOrNil`.
 Chain.first    = Chain.firstOrNil
+--- Alias for `:getByKeyOrNil`.
 Chain.get      = Chain.getByKeyOrNil
+--- Alias for `:lastOrNil`.
 Chain.last     = Chain.lastOrNil
+--- Alias for `:count`.
 Chain.len      = Chain.count
+--- Alias for `:count`.
 Chain.length   = Chain.count
+--- Alias for `:singleOrNil`.
 Chain.single   = Chain.singleOrNil
+--- Alias for `:count`.
 Chain.size     = Chain.count
+--- Alias for `:luaSlice`.
 Chain.slice    = Chain.luaSlice
+--- Alias for `:distinct`.
 Chain.unique   = Chain.distinct
+--- Alias for `:getByValueOrNil`.
 Chain.val      = Chain.getByValueOrNil
---- / Aliases
+---- / Aliases
 
 return _chain(chain)
--- src/aliases.lua
+++ src/aliases.lua
@@ -1,19 +1,35 @@
 local Chain = require("chain_class")
 
---- Aliases
+---- Aliases
+----
+--- Alias for `:getAtPositionOrNil`.
 Chain.at       = Chain.getAtPositionOrNil
+--- Alias for `:forEach`.
 Chain.each     = Chain.forEach
+--- Alias for `:forEach`.
 Chain.foreach  = Chain.forEach
+--- Alias for `:findFirstOrNil`.
 Chain.find     = Chain.findFirstOrNil
+--- Alias for `:findLastOrNil`.
 Chain.findLast = Chain.findLastOrNil
+--- Alias for `:firstOrNil`.
 Chain.first    = Chain.firstOrNil
+--- Alias for `:getByKeyOrNil`.
 Chain.get      = Chain.getByKeyOrNil
+--- Alias for `:lastOrNil`.
 Chain.last     = Chain.lastOrNil
+--- Alias for `:count`.
 Chain.len      = Chain.count
+--- Alias for `:count`.
 Chain.length   = Chain.count
+--- Alias for `:singleOrNil`.
 Chain.single   = Chain.singleOrNil
+--- Alias for `:count`.
 Chain.size     = Chain.count
+--- Alias for `:luaSlice`.
 Chain.slice    = Chain.luaSlice
+--- Alias for `:distinct`.
 Chain.unique   = Chain.distinct
+--- Alias for `:getByValueOrNil`.
 Chain.val      = Chain.getByValueOrNil
---- / Aliases
+---- / Aliases
